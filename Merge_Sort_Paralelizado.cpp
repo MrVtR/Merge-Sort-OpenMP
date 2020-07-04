4d10ea64eedd611457e93a50537d6f496c4e533b
MrVtR <30987772+MrVtR@users.noreply.github.com>
2020-07-03 21:16:44 -0300

    Merge Sort - Feito em C++

---
+++ Merge_Sort_Paralelizado.cpp
@@ -0,0 +1,101 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include<chrono>
+#include<omp.h>
+
+#define n 1000000
+void mergeSort(int a[],int l,int r);
+void merge(int a,int l,int m,int r);
+void mergeSortSequencial(int a[],int l,int r);
+
+
+
+int main()
+{
+    int a[n],i;
+    srand(time(0));// Função Srand usada para que o código randomize o vetor de forma diferente a cada execução
+    omp_set_num_threads(2);
+    for(i = 0; i <n; i++)
+    {
+        a[i]=rand() % 2000000; //Randomizar vetor com valores inteiros até 2 milhões
+    }
+    auto inicio = std::chrono::high_resolution_clock::now();//Clock de início
+
+    #pragma omp parallel // Definição da reguão paralela(Envolve todo o mergeSort)
+    {
+        #pragma omp single
+        mergeSort(a, 0, n-1);
+    }     
+
+    auto resultado = std::chrono::high_resolution_clock::now() - inicio;// Clock final
+    long long milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(resultado).count();
+     
+    //Print do Vetor ordenado para testes de conferência
+    /*for(i = 0; i <n; i++)
+          printf("%d ", a[i]);
+    printf("\n");*/
+    printf("Tempo do Merge: 0.%lld ms\n",milliseconds);
+    return(0);
+
+}
+void merge(int a[],int l,int m,int r)
+{
+    int b[m-l+1], c[r-m];
+    for(int i=0; i<(m-l+1); i++){
+        b[i]=a[l+i];
+    }
+    for(int i=0; i<(r-m); i++){
+        c[i]=a[m+1+i];
+    }
+    int i=0, j=0, k=l;
+    while(i<(m-l+1) && j<(r-m))
+    {
+        if(b[i]<c[j]){
+            a[k++]=b[i++];
+        }
+        else{
+            a[k++]=c[j++];
+        }
+    }
+    while(i<(m-l+1)){
+        a[k++]=b[i++];
+    }
+    while(j<(r-m)){
+        a[k++]=c[j++];
+    }
+}
+
+void mergeSortSequencial(int a[],int l,int r)
+{
+    if (l<r)
+    {
+        int m=(l + r)/2;
+        mergeSortSequencial(a,l,m); 
+        mergeSortSequencial(a,m+1,r);
+        merge(a,l,m,r);
+    }
+}
+void mergeSort(int a[],int l,int r)
+{
+    if (l<r)
+    {
+        if ((r-l)>1000) // Condição de parada do Paralelismo,se houver menos que 1000 ordenações a serem feitas,
+                        // volta pro sequencial,pois não compensa gastar processamento
+        {
+            int m=(l+r)/2;
+            // Diretiva firstprivate para ser possível inicializar a mesma variável sem corrida de dados
+            // Comandos de Task utilizados em função da recursão do MergeSort
+            #pragma omp task firstprivate (a,l,m)
+                mergeSort(a,l,m); 
+            #pragma omp task firstprivate (a,m,r)
+                mergeSort(a,m+1,r);
+            #pragma omp taskwait //Diretiva taskwait para esperar todos as chamadas recursivas serem feitas
+                                // e após isso, ordenar o vetor de uma só vez
+            merge(a,l,m,r);
+        } 
+        else
+            mergeSortSequencial(a,l,r); //Chamada da função sequencial quando não há ganhos mais em se paralelizar
+    }
+}
+
